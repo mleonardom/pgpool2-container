4a8449797afc06920d5fa96a266a0b2e299da0b3
Jonathan DeMarks <ca0abinary@users.noreply.github.com>
2017-02-20 11:20:51 -0500

    Fix for * listener in pgpool.conf
    http://www.pgpool.net/mantisbt/view.php?id=202

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,7 +9,7 @@ if [ "$1" = 'pgpool' ]; then
   sed -i "s:socket_dir = '.*':socket_dir = '/var/run/pgpool':g" /etc/pgpool2/pgpool.conf
   sed -i "s:pcp_socket_dir = '.*':pcp_socket_dir = '/var/run/pgpool':g" /etc/pgpool2/pgpool.conf
   #IP_ADDR=$(ip addr show eth0 | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
-  #sed -i "s:listen_addresses = '.*':listen_addresses = '$IP_ADDR':g" /etc/pgpool2/pgpool.conf
+  sed -i "s:listen_addresses = '.*':listen_addresses = '0.0.0.0':g" /etc/pgpool2/pgpool.conf
 
   gosu postgres "$@"
   
